b20ba5a1b3ec146bfe143b2eb7c10579907b46e5
Mukti Bafna <muktibafna30@gmail.com>
2026-05-06 16:37:57 +0530

    Final responsive UI fixes

--- init/index.js
+++ init/index.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
+if(process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
 .then(() => {
